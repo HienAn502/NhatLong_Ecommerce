9bc590bfa5b5d77e24e26e6f2b6422106b3f03a4
annh502 <annh502@rabiloo.com>
2023-12-03 15:16:37 +0700

    commit

--- user/views.py
+++ user/views.py
@@ -1,4 +1,8 @@
+from django.core.paginator import Paginator
+from django.http import JsonResponse
 from django.shortcuts import render
+
+from custom_admin.models import Product, Category
 
 
 # Create your views here.
@@ -13,22 +17,62 @@ def get_list_product(request):
         TODO:
             return list bao gồm tất cả các product
     '''
+    products = Product.objects.all()
+    get_list_product = []
+    for product in products:
+        get_list_product.append(product.to_dict())
 
+    get_list_product = [product.to_dict() for product in products]
+    return JsonResponse(get_list_product, status=200)
+def get_list_category(request):
+    products = Product.objects.all()
+    get_list_category = []
+    for product in products:
+        get_list_category.append(product.to_dict())
+
+    get_list_category = [product.to_dict() for product in products]
+    return JsonResponse(get_list_category, status=200)
 
 def get_page_product(request, page=1, size=10):
     '''
         TODO:
             return trang tất cả product nhưng bao gồm phân trang
             product.html
+
     '''
-
-
+    products = Product.objects.all()
+    paginator = Paginator(products, size)
+    page_obj = paginator.get_page(page)
+    response_data = {
+        "total_elements": paginator.count,
+        "total_pages": paginator.num_pages,
+        "current_page": page,
+        "content": [product.to_dict() for product in page_obj.object_list]
+    }
+    return JsonResponse(response_data, status=200)
+def get_page_category(request, page=2, size=10 ):
+    categorys = Category.objects.all()
+    paginator = Paginator(categorys, size)
+    page_obj = paginator.get_page(page)
+    response_data = {
+        "total_elements": paginator.count,
+        "total_pages": paginator.num_pages,
+        "current_page": page,
+        "content": [category.to_dict() for category in page_obj.object_list]
+    }
+    return JsonResponse(response_data, status=200)
 def get_product(request, product_id):
     '''
         TODO:
             return product với product_id tương ứng
     '''
+    products = Product.product_id.all()
+    response_data = {
+        "total_elements": product_id.count,
 
+        "content": [product.to_dict() for product in product_id.object_list]
+    }
+    return JsonResponse(response_data, status=200)
 
 def add_cart_item(request, product_id):
     '''
