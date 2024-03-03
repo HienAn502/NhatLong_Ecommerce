f4663a6414fb24caccdbf6346554d32e5363d250
annh502 <annh502@rabiloo.com>
2024-03-03 14:21:42 +0700

    b8

--- custom_admin/urls.py
+++ custom_admin/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path("", views.admin_dashboard, name='admin_dashboard'),
     path("product/page/", views.get_page_product, name='admin_page_product'),
     path("category/page/", views.get_page_category, name='admin_page_category'),
+    path("product/<int:product_id>", views.edit_product, name="edit_product"),
 
     path("logout/", views.logout, name='logout'),
 
